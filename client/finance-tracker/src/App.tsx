894221b52656b76f14130a19c26c1ae419229804
dsaladbar617 <dsalazar219@gmail.com>
2024-04-05 14:39:42 -0500

    basic styling for header and main body of page

--- client/finance-tracker/src/App.tsx
+++ client/finance-tracker/src/App.tsx
@@ -1,11 +1,16 @@
+import { Route } from "wouter";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
-      <div>Yo</div>
+      <main>
+        <Route path="/" component={Home} />
+        <Route path="/yo" component={() => <div>Yo</div>} />
+      </main>
     </>
   );
 }
